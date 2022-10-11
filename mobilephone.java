34ec85795c1c72ca4303c5cc88f15a6763dfa095
Janani-01 <115526516+Janani-01@users.noreply.github.com>
2022-10-11 11:46:28 +0530

    Add files via upload
    week1

---
+++ mobilephone.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class mobilephone {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String mobileBrandName= "nothing";
+System.out.println(mobileBrandName);
+char mobileLogo=   'N';
+System.out.println(mobileLogo);
+short noOfMobilePiece= 30;
+System.out.println(noOfMobilePiece);
+int mobileNumber= 9847;
+System.out.println(mobileNumber);
+long mobileImeiNumber= 9876543219L;
+System.out.println(mobileImeiNumber);
+float mobilePrice=45999.97F;
+System.out.println(mobilePrice);
+boolean isDamaged= false;
+System.out.println(isDamaged);
+}
+
+}
---
+++ twowheeler.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class twowheeler {
+		public static void main(String[] args) {
+		int noOfWheels=2;
+		System.out.println(noOfWheels);
+		 short noOfMirrors=2;
+		 System.out.println(noOfMirrors);
+		 long chassisNumber=8646L;
+		 System.out.println(chassisNumber);
+		 boolean isPunctured = false;
+		 System.out.println(isPunctured);
+		 double bikeKM=4987.4;
+		 System.out.println(bikeKM);
+		 String bikeName= "royal enfield";
+		 System.out.println(bikeName);
+		 
+			}
+		}
+
